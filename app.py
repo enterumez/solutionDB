30d79fd86d64a471f8e63471c48820e5af67e791
enterumez <131345035+enterumez@users.noreply.github.com>
2024-07-06 22:36:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -69,17 +69,6 @@ def delete_post(post_id):
         st.write(e)
 
 # Define some HTML templates for displaying the posts
-title_temp = """
-<div style="background-color:#c7c9cf;padding:10px;border-radius:10px;margin:10px;">
-<h4 style="color:black;text-align:center;">{}</h4>
-<img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style="vertical-align: middle;float:left;width: 50px;height: 50px;border-radius: 50%;">
-<h6>Author: {}</h6>
-<br/>
-<br/>
-<p style="text-align:justify"> {}</p>
-</div>
-"""
-
 post_temp = """
 <div style="background-color:#c7c9cf;padding:10px;border-radius:5px;margin:10px;">
 <h4 style="color:black;text-align:center;">{}</h4>
@@ -96,8 +85,12 @@ post_temp = """
 delete_password = "shuta0105"
 create_password = "shuta0105"
 
+# Get all the posts to create dynamic menu
+posts = get_all_posts()
+titles = [post[2] for post in posts]  # Extract titles for the menu
+
 # Create a sidebar menu with different options
-menu = ["Home", "View Posts", "Add Post", "Manage"]
+menu = ["Home", "Add Post", "Manage"] + titles
 choice = st.sidebar.selectbox("Menu", menu)
 
 # Display the selected option
@@ -108,18 +101,14 @@ if choice == "Home":
     image = Image.open('Image/図2.png')
     st.image(image)
 
-elif choice == "View Posts":
-    st.title("View Posts")
-    st.write("Here you can see all the posts in the blog.")
-    # Get all the posts from the database
-    posts = get_all_posts()
-    # Display each post as a card
+elif choice in titles:
+    st.title(choice)
+    # Find the post details by title
     for post in posts:
-        st.markdown(title_temp.format(post[2], post[1], post[3][:10] + "..."), unsafe_allow_html=True)
-        # Add a button to view the full post
-        button_key = f"read_more_{post[0]}"  # Generate a unique key here
-        if st.button("Read More", key=button_key):
+        if post[2] == choice:
             st.markdown(post_temp.format(post[2], post[1], post[4], post[3]), unsafe_allow_html=True)
+            break
+
 elif choice == "Add Post":
     st.title("Add Post")
     st.write("Here you can add a new post to the blog.")
@@ -136,13 +125,14 @@ elif choice == "Add Post":
         if password == create_password:
             add_post(author, title, content, date)
             st.success("Post added successfully")
+            st.experimental_rerun()  # Refresh the page to update the menu
         else:
             st.error("Invalid password")
+
 elif choice == "Manage":
     st.title("Manage")
     st.write("Here you can delete posts or view some statistics.")
     # Create a selectbox to choose a post to delete
-    posts = get_all_posts()
     titles = [f"{post[0]}: {post[2]}" for post in posts]  # Display post ID and title
     selected = st.selectbox("Select a post to delete", titles)
     post_id = int(selected.split(":")[0])  # Extract post ID
@@ -153,6 +143,7 @@ elif choice == "Manage":
         if password == delete_password:
             delete_post(post_id)
             st.success("Post deleted successfully")
+            st.experimental_rerun()  # Refresh the page to update the menu
         else:
             st.error("Invalid password")
     # Create a checkbox to show some statistics
